08fec3c5e8962ca0eaad94b6799ce1d4a42f926c
James Lippitt <james.lippitt@gmail.com>
2023-05-14 11:24:33 +0100

    Load game data into memory

--- sdl/src/main.rs
+++ sdl/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::error::Error;
+use std::fs;
+
+#[derive(Parser, Debug)]
+#[command(author, version)]
+struct Args {
+    rom_path: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let rom_data = fs::read(args.rom_path)?;
+
+    println!("{:?}", rom_data);
+
+    Ok(())
 }
