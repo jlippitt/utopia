b8a66e74e18eccf954d880fdabffa4cb61257f05
James Lippitt <james.lippitt@gmail.com>
2023-06-22 10:53:04 +0100

    Implement PPU address registers

--- utopia/src/system/nes/ppu.rs
+++ utopia/src/system/nes/ppu.rs
@@ -7,11 +7,19 @@ const TOTAL_LINES: u32 = 262;
 
 const DOTS_PER_LINE: u32 = 341;
 
+struct Registers {
+    v: u16,
+    t: u16,
+    x: u8,
+    w: bool,
+}
+
 pub struct Ppu {
     line: u32,
     dot: u32,
     nmi_occurred: bool,
     nmi_active: bool,
+    regs: Registers,
 }
 
 impl Ppu {
@@ -21,6 +29,12 @@ impl Ppu {
             dot: 0,
             nmi_occurred: false,
             nmi_active: false,
+            regs: Registers {
+                v: 0,
+                t: 0,
+                x: 0,
+                w: false,
+            }
         }
     }
 
@@ -41,6 +55,8 @@ impl Ppu {
                     *interrupt &= !INT_NMI;
                 }
 
+                self.regs.w = false;
+
                 result
             }
             _ => panic!("PPU read {:04X} not yet implemented", address),
@@ -60,6 +76,35 @@ impl Ppu {
 
                 self.nmi_active = nmi_active;
                 debug!("NMI Active: {}", self.nmi_active);
+
+                self.regs.t = (self.regs.t & 0x73ff) | ((value as u16 & 0x03) << 10);
+                debug!("PPU TMP Address: {:04X}", self.regs.t);
+            }
+            5 => {
+                if self.regs.w {
+                    self.regs.t = (self.regs.t & 0x0c1f) | ((value as u16 & 0xf8) << 2) | ((value as u16 & 0x07) << 12);
+                    debug!("PPU TMP Address: {:04X}", self.regs.t);
+                } else {
+                    self.regs.t = (self.regs.t & 0x7fe0) | ((value >> 3) as u16);
+                    self.regs.x = value & 0x07;
+                    debug!("PPU TMP Address: {:04X}", self.regs.t);
+                    debug!("PPU Fine X: {}", self.regs.x);
+                }
+
+                self.regs.w = !self.regs.w;
+            },
+            6 => {
+                if self.regs.w {
+                    self.regs.t = (self.regs.t & 0xff00) | value as u16;
+                    self.regs.v = self.regs.t;
+                    debug!("PPU TMP Address: {:04X}", self.regs.t);
+                    debug!("PPU VRAM Address: {:04X}", self.regs.v);
+                } else {
+                    self.regs.t = (self.regs.t & 0xff) | ((value as u16 & 0x3f) << 8);
+                    debug!("PPU TMP Address: {:04X}", self.regs.t);
+                }
+
+                self.regs.w = !self.regs.w;
             }
             _ => warn!("PPU write {:04X} not yet implemented", address),
         }
