099004be93ba28b0f4e0146e046f8540b44dcf3a
James Lippitt <james.lippitt@gmail.com>
2023-06-27 12:40:03 +0100

    Reduce logging by default in release mode

--- utopia-sdl/src/log.rs
+++ utopia-sdl/src/log.rs
@@ -1,7 +1,4 @@
 use std::fmt;
-use std::fs::{self, File};
-use std::io::{self, BufWriter};
-use std::sync::Mutex;
 use tracing::event::Event;
 use tracing::subscriber::{DefaultGuard, Subscriber};
 use tracing_subscriber::filter::{EnvFilter, LevelFilter};
@@ -10,8 +7,6 @@ use tracing_subscriber::fmt::{
     FmtContext, MakeWriter,
 };
 use tracing_subscriber::registry::LookupSpan;
-
-const LOG_BUFFER_SIZE: usize = 262144;
 
 pub struct Formatter;
 
@@ -31,21 +26,18 @@ where
     }
 }
 
-pub type DebugWriter = Mutex<BufWriter<File>>;
-
-pub fn create_debug_writer(name: &str) -> io::Result<DebugWriter> {
-    fs::create_dir_all("./log")?;
-    let file = File::create(format!("./log/{}.log", name))?;
-    let buf_writer = BufWriter::with_capacity(LOG_BUFFER_SIZE, file);
-    Ok(Mutex::new(buf_writer))
-}
-
 pub fn set_subscriber<W>(writer: W) -> DefaultGuard
 where
     W: for<'writer> MakeWriter<'writer> + 'static + Send + Sync,
 {
+    #[cfg(debug_assertions)]
+    let default_level = LevelFilter::DEBUG;
+
+    #[cfg(not(debug_assertions))]
+    let default_level = LevelFilter::INFO;
+
     let env_filter = EnvFilter::builder()
-        .with_default_directive(LevelFilter::DEBUG.into())
+        .with_default_directive(default_level.into())
         .from_env_lossy();
 
     let subscriber = tracing_subscriber::fmt()
@@ -56,3 +48,26 @@ where
 
     tracing::subscriber::set_default(subscriber)
 }
+
+#[cfg(debug_assertions)]
+mod debug {
+    use std::fs::{self, File};
+
+    use std::io::{self, BufWriter};
+
+    use std::sync::Mutex;
+
+    const LOG_BUFFER_SIZE: usize = 262144;
+
+    pub type DebugWriter = Mutex<BufWriter<File>>;
+
+    pub fn create_debug_writer(name: &str) -> io::Result<DebugWriter> {
+        fs::create_dir_all("./log")?;
+        let file = File::create(format!("./log/{}.log", name))?;
+        let buf_writer = BufWriter::with_capacity(LOG_BUFFER_SIZE, file);
+        Ok(Mutex::new(buf_writer))
+    }
+}
+
+#[cfg(debug_assertions)]
+pub use debug::*;
