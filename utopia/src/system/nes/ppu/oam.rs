e2fa1f5622d12fd56115c9c098cf359ba48231e3
James Lippitt <james.lippitt@gmail.com>
2023-06-27 13:33:06 +0100

    NES: OAM reads and writes

--- utopia/src/system/nes/ppu/oam.rs
+++ utopia/src/system/nes/ppu/oam.rs
@@ -1,12 +1,16 @@
-use tracing::{debug, warn};
+use tracing::debug;
 
 pub struct Oam {
     address: u8,
+    data: [u8; 256],
 }
 
 impl Oam {
     pub fn new() -> Self {
-        Self { address: 0 }
+        Self {
+            address: 0,
+            data: [0; 256],
+        }
     }
 
     pub fn set_address(&mut self, value: u8) {
@@ -14,11 +18,15 @@ impl Oam {
         debug!("OAM Address: {:02X}", self.address);
     }
 
-    pub fn read(&mut self) -> u8 {
-        panic!("OAM reads not yet implemented");
+    pub fn read(&self) -> u8 {
+        let value = self.data[self.address as usize];
+        debug!("OAM Read: {:02X} => {:02X}", self.address, value);
+        value
     }
 
-    pub fn write(&mut self, _value: u8) {
-        warn!("OAM writes not yet implemented");
+    pub fn write(&mut self, value: u8) {
+        debug!("OAM Write: {:02X} <= {:02X}", self.address, value);
+        self.data[self.address as usize] = value;
+        self.address = self.address.wrapping_add(1);
     }
 }
