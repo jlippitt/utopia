4df71b2656e8e844f287b805216dea610f8d3fd0
James Lippitt <james.lippitt@gmail.com>
2023-06-28 10:00:38 +0100

    UxROM fixes

--- utopia/src/system/nes/cartridge/mapper/uxrom.rs
+++ utopia/src/system/nes/cartridge/mapper/uxrom.rs
@@ -1,4 +1,5 @@
 use super::{Mapper, Mappings};
+use tracing::debug;
 
 const PRG_BANK_SIZE: usize = 16384;
 
@@ -15,11 +16,14 @@ impl UxROM {
 impl Mapper for UxROM {
     fn init_mappings(&mut self, mappings: &mut Mappings) {
         mappings.map_prg_rom(8, 4, 0);
-        mappings.map_prg_rom(8, 4, self.prg_rom_size - PRG_BANK_SIZE);
+        mappings.map_prg_rom(12, 4, self.prg_rom_size - PRG_BANK_SIZE);
         mappings.map_registers(8, 8);
+        debug!("UxROM PRG Read Mapping: {:?}", mappings.prg_read);
+        debug!("UxROM PRG Write Mapping: {:?}", mappings.prg_write);
     }
 
     fn write_register(&mut self, mappings: &mut Mappings, _address: u16, value: u8) {
         mappings.map_prg_rom(8, 4, PRG_BANK_SIZE * (value as usize & 0x0f));
+        debug!("UxROM PRG Read Mapping: {:?}", mappings.prg_read);
     }
 }
