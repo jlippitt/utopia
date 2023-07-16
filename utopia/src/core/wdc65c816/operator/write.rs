5798ca6b1cbbf1bf4c2cdd6e642dd54636062a05
James Lippitt <james.lippitt@gmail.com>
2023-07-16 09:59:48 +0100

    Implement STA/STX/STY

--- utopia/src/core/wdc65c816/operator/write.rs
+++ utopia/src/core/wdc65c816/operator/write.rs
@@ -6,6 +6,48 @@ pub trait WriteOperator {
     fn apply16(core: &Core<impl Bus>) -> u16;
 }
 
+pub struct Sta;
+
+impl WriteOperator for Sta {
+    const NAME: &'static str = "STA";
+
+    fn apply8(core: &Core<impl Bus>) -> u8 {
+        core.a as u8
+    }
+
+    fn apply16(core: &Core<impl Bus>) -> u16 {
+        core.a
+    }
+}
+
+pub struct Stx;
+
+impl WriteOperator for Stx {
+    const NAME: &'static str = "STX";
+
+    fn apply8(core: &Core<impl Bus>) -> u8 {
+        core.x as u8
+    }
+
+    fn apply16(core: &Core<impl Bus>) -> u16 {
+        core.x
+    }
+}
+
+pub struct Sty;
+
+impl WriteOperator for Sty {
+    const NAME: &'static str = "STY";
+
+    fn apply8(core: &Core<impl Bus>) -> u8 {
+        core.y as u8
+    }
+
+    fn apply16(core: &Core<impl Bus>) -> u16 {
+        core.y
+    }
+}
+
 pub struct Stz;
 
 impl WriteOperator for Stz {
