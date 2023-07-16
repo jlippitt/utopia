c74fc66de65a27305dd639e2746e20101266ed87
James Lippitt <james.lippitt@gmail.com>
2023-07-16 10:43:05 +0100

    Implement TCS and TSC

--- utopia/src/core/wdc65c816/instruction/register.rs
+++ utopia/src/core/wdc65c816/instruction/register.rs
@@ -1,4 +1,4 @@
-use super::super::{Bus, Core};
+use super::super::{Bus, Core, EMULATION_STACK_PAGE};
 use tracing::debug;
 
 pub fn tcd(core: &mut Core<impl Bus>) {
@@ -14,5 +14,25 @@ pub fn tdc(core: &mut Core<impl Bus>) {
     core.poll();
     core.idle();
     core.a = core.d;
+    core.set_nz16(core.a);
+}
+
+pub fn tcs<const E: bool>(core: &mut Core<impl Bus>) {
+    debug!("TCS");
+    core.poll();
+    core.idle();
+
+    if E {
+        core.s = EMULATION_STACK_PAGE | (core.a & 0xff);
+    } else {
+        core.s = core.a;
+    }
+}
+
+pub fn tsc(core: &mut Core<impl Bus>) {
+    debug!("TSC");
+    core.poll();
+    core.idle();
+    core.a = core.s;
     core.set_nz16(core.a);
 }
