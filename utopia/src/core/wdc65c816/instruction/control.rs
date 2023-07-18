84f6cca9c69156e36ecf5a1493d6c1287ccc91d4
James Lippitt <james.lippitt@gmail.com>
2023-07-18 09:40:51 +0100

    Implement RTS

--- utopia/src/core/wdc65c816/instruction/control.rs
+++ utopia/src/core/wdc65c816/instruction/control.rs
@@ -12,3 +12,14 @@ pub fn jsr<const E: bool>(core: &mut Core<impl Bus>) {
     core.push::<E>(core.pc as u8);
     core.pc = (core.pc & 0xffff0000) | (target as u32);
 }
+
+pub fn rts<const E: bool>(core: &mut Core<impl Bus>) {
+    debug!("RTS");
+    core.idle();
+    core.idle();
+    let low = core.pull::<E>();
+    let high = core.pull::<E>();
+    let target = u16::from_le_bytes([low, high]).wrapping_add(1);
+    core.pc = (core.pc & 0xffff0000) | (target as u32);
+    core.idle();
+}
