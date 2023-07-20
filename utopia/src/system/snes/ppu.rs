b44cb198f47d307118f0810740ffb23241bb25fd
James Lippitt <james.lippitt@gmail.com>
2023-07-20 10:12:36 +0100

    Implement CGRAM writes

--- utopia/src/system/snes/ppu.rs
+++ utopia/src/system/snes/ppu.rs
@@ -1,16 +1,21 @@
+use cgram::Cgram;
 use tracing::warn;
-
 use vram::Vram;
 
+mod cgram;
 mod vram;
 
 pub struct Ppu {
     vram: Vram,
+    cgram: Cgram,
 }
 
 impl Ppu {
     pub fn new() -> Self {
-        Self { vram: Vram::new() }
+        Self {
+            vram: Vram::new(),
+            cgram: Cgram::new(),
+        }
     }
 
     pub fn read(&mut self, address: u8) -> u8 {
@@ -26,6 +31,8 @@ impl Ppu {
             0x17 => self.vram.set_address_high(value),
             0x18 => self.vram.write_low(value),
             0x19 => self.vram.write_high(value),
+            0x21 => self.cgram.set_address(value),
+            0x22 => self.cgram.write(value),
             _ => warn!("Unmapped PPU write: {:02X} <= {:02X}", address, value),
         }
     }
---
+++ utopia/src/system/snes/ppu/cgram.rs
@@ -0,0 +1,41 @@
+use crate::util::MirrorVec;
+use tracing::debug;
+
+const CGRAM_SIZE: usize = 128;
+
+pub struct Cgram {
+    data: MirrorVec<u16>,
+    address: u8,
+    high_byte: bool,
+    buffer: u8,
+}
+
+impl Cgram {
+    pub fn new() -> Self {
+        Self {
+            data: MirrorVec::new(CGRAM_SIZE),
+            address: 0,
+            high_byte: false,
+            buffer: 0,
+        }
+    }
+
+    pub fn set_address(&mut self, value: u8) {
+        self.address = value;
+        self.high_byte = false;
+        debug!("CGRAM Address: {:02X}", self.address);
+    }
+
+    pub fn write(&mut self, value: u8) {
+        if self.high_byte {
+            let value = ((value as u16 & 0x7f) << 8) | (self.buffer as u16);
+            self.data[self.address as usize] = value;
+            debug!("CGRAM Write: {:02X} <= {:04X}", self.address, value);
+            self.address = self.address.wrapping_add(1);
+        } else {
+            self.buffer = value;
+        }
+
+        self.high_byte = !self.high_byte;
+    }
+}
