5efb4b2d979af9ee6d12cba7a62242d1d646bde7
James Lippitt <james.lippitt@gmail.com>
2023-07-25 07:34:02 +0100

    Add HiROM mapping

--- utopia/src/system/snes/memory.rs
+++ utopia/src/system/snes/memory.rs
@@ -36,7 +36,7 @@ pub fn map(header: &Header) -> [Page; TOTAL_PAGES] {
 
     match header.map_mode & 0x0f {
         0x00 => map_lo_rom(&mut pages, header),
-        0x01 => todo!("HiROM"),
+        0x01 => map_hi_rom(&mut pages, header),
         _ => unimplemented!("Map Mode {:02X}", header.map_mode),
     }
 
@@ -92,6 +92,43 @@ fn map_lo_rom(pages: &mut [Page], header: &Header) {
             pages[index | 1] = Page::Sram(offset | 0x2000);
             pages[index | 2] = Page::Sram(offset | 0x4000);
             pages[index | 3] = Page::Sram(offset | 0x6000);
+        }
+    }
+
+    pages.copy_within(0..(TOTAL_PAGES / 2), TOTAL_PAGES / 2);
+}
+
+fn map_hi_rom(pages: &mut [Page], header: &Header) {
+    let rom_size = header.rom_size;
+    let sram_size = header.sram_size;
+
+    for bank in 0x00..=0x3f {
+        let index = bank << 3;
+        let offset = (bank as usize) << 16;
+        pages[index | 4] = Page::Rom(mirror(rom_size, offset | 0x8000) as u32);
+        pages[index | 5] = Page::Rom(mirror(rom_size, offset | 0xa000) as u32);
+        pages[index | 6] = Page::Rom(mirror(rom_size, offset | 0xc000) as u32);
+        pages[index | 7] = Page::Rom(mirror(rom_size, offset | 0xe000) as u32);
+    }
+
+    for bank in 0x40..=0x7f {
+        let index = bank << 3;
+        let offset = (bank - 0x40 as usize) << 16;
+        pages[index | 0] = Page::Rom(mirror(rom_size, offset | 0x0000) as u32);
+        pages[index | 1] = Page::Rom(mirror(rom_size, offset | 0x2000) as u32);
+        pages[index | 2] = Page::Rom(mirror(rom_size, offset | 0x4000) as u32);
+        pages[index | 3] = Page::Rom(mirror(rom_size, offset | 0x6000) as u32);
+        pages[index | 4] = Page::Rom(mirror(rom_size, offset | 0x8000) as u32);
+        pages[index | 5] = Page::Rom(mirror(rom_size, offset | 0xa000) as u32);
+        pages[index | 6] = Page::Rom(mirror(rom_size, offset | 0xc000) as u32);
+        pages[index | 7] = Page::Rom(mirror(rom_size, offset | 0xe000) as u32);
+    }
+
+    if sram_size > 0 {
+        for bank in 0x20..=0x3f {
+            let index = (bank as usize) << 3;
+            let offset = (bank - 0x20 as u32) << 13;
+            pages[index | 3] = Page::Sram(offset);
         }
     }
 
