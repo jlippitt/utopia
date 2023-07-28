8a75e8bbe02001a6c406c80f68cdbae401e89f76
James Lippitt <james.lippitt@gmail.com>
2023-07-28 18:54:05 +0100

    Implement SLTI

--- utopia/src/core/mips/instruction/alu.rs
+++ utopia/src/core/mips/instruction/alu.rs
@@ -28,6 +28,17 @@ pub fn addiu(core: &mut Core<impl Bus>, rs: usize, rt: usize, value: u32) {
     core.set(rt, result);
 }
 
+pub fn slti(core: &mut Core<impl Bus>, rs: usize, rt: usize, value: u32) {
+    debug!(
+        "{:08X} SLTI {}, {}, {}",
+        core.pc, REGS[rt], REGS[rs], value as i16
+    );
+
+    let ivalue = value as i16 as i32;
+    let result = (core.get(rs) as i32).wrapping_sub(ivalue);
+    core.set(rt, (result < 0) as u32);
+}
+
 pub fn ori(core: &mut Core<impl Bus>, rs: usize, rt: usize, value: u32) {
     debug!(
         "{:08X} ORI {}, {}, 0x{:04X}",
