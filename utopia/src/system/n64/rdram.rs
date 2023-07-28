8a3577222f2df99a8de2b29d0ba879835262ae57
James Lippitt <james.lippitt@gmail.com>
2023-07-28 14:34:48 +0100

    Write RI_SELECT register

--- utopia/src/system/n64/rdram.rs
+++ utopia/src/system/n64/rdram.rs
@@ -1,7 +1,8 @@
 use crate::util::facade::{DataReader, DataWriter};
+use tracing::debug;
 
 pub struct Interface {
-    //
+    ri_select: u32,
 }
 
 pub struct Rdram {
@@ -11,7 +12,7 @@ pub struct Rdram {
 impl Rdram {
     pub fn new() -> Self {
         Self {
-            interface: Interface {},
+            interface: Interface { ri_select: 0 },
         }
     }
 
@@ -28,18 +29,22 @@ impl DataReader for Interface {
     type Address = u32;
     type Value = u32;
 
-    fn read(&self, _address: u32) -> u32 {
-        // TODO
-        0
+    fn read(&self, address: u32) -> u32 {
+        match address {
+            0x0c => self.ri_select,
+            _ => unimplemented!("RDRAM Interface Read: {:08X}", address),
+        }
     }
 }
 
 impl DataWriter for Interface {
     fn write(&mut self, address: u32, value: u32) {
         match address {
-            0x04 => {
-                // RI_CONFIG
-                // Ignore
+            0x04 => (), // RI_CONFIG: Ignore
+            0x08 => (), // RI_CURRENT_LOAD: Ignore
+            0x0c => {
+                self.ri_select = value & 0xff;
+                debug!("RI_SELECT: {:08X}", value);
             }
             _ => unimplemented!("RDRAM Interface Write: {:08X} <= {:08X}", address, value),
         }
