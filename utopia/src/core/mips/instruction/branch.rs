547c60fce2252df911975c63392b09dbff7a8744
James Lippitt <james.lippitt@gmail.com>
2023-07-28 18:09:45 +0100

    Debug should show shifted offsets for branch instructions

--- utopia/src/core/mips/instruction/branch.rs
+++ utopia/src/core/mips/instruction/branch.rs
@@ -2,15 +2,13 @@ use super::super::{Bus, Core, REGS};
 use tracing::debug;
 
 pub fn bne(core: &mut Core<impl Bus>, rs: usize, rt: usize, value: u32) {
-    debug!(
-        "{:08X} BNE {}, {}, {}",
-        core.pc, REGS[rt], REGS[rs], value as i16
-    );
+    let offset = (value as i16 as i32) << 2;
+
+    debug!("{:08X} BNE {}, {}, {}", core.pc, REGS[rt], REGS[rs], offset);
 
     if core.get(rs) != core.get(rt) {
         debug!("  Branch taken");
-        let ivalue = ((value as i16 as i32) << 2) as u32;
-        core.next[1] = core.next[0].wrapping_add(ivalue);
+        core.next[1] = core.next[0].wrapping_add(offset as u32);
     } else {
         debug!("  Branch not taken");
     }
