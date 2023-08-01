a7dd87fff023345f1ac5159500ae5799db69d03c
James Lippitt <james.lippitt@gmail.com>
2023-08-01 11:31:24 +0100

    Implement BX for Thumb mode

--- utopia/src/core/arm7tdmi/thumb/control.rs
+++ utopia/src/core/arm7tdmi/thumb/control.rs
@@ -1,5 +1,5 @@
 use super::super::condition::Condition;
-use super::super::{Bus, Core};
+use super::super::{Bus, Core, REGS};
 use num_traits::FromPrimitive;
 use tracing::debug;
 
@@ -20,4 +20,20 @@ pub fn branch_conditional(core: &mut Core<impl Bus>, pc: u32, word: u16) {
     let offset = ((word & 0xff) as i8 as i32) << 1;
     debug!("{:08X} B {:+}", pc, offset);
     core.pc = core.pc.wrapping_add(2).wrapping_add(offset as u32);
+}
+
+pub fn bx(core: &mut Core<impl Bus>, pc: u32, word: u16) {
+    let rs = ((word >> 3) & 15) as usize;
+
+    debug!("{:08X} BX {}", pc, REGS[rs]);
+
+    let target = core.get(rs);
+    core.pc = target & 0xffff_fffe;
+    core.cpsr.t = (target & 0x0000_0001) != 0;
+
+    if core.cpsr.t {
+        debug!("  Thumb Mode");
+    } else {
+        debug!("  ARM Mode");
+    }
 }
