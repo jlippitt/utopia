bd94a7ecd8f8e98e8224177e2e5165e754e97206
James Lippitt <james.lippitt@gmail.com>
2023-08-01 09:23:32 +0100

    Implement 3-operand ADD

--- utopia/src/core/arm7tdmi/thumb.rs
+++ utopia/src/core/arm7tdmi/thumb.rs
@@ -14,6 +14,10 @@ pub fn dispatch(core: &mut Core<impl Bus>) {
     core.pc = core.pc.wrapping_add(2);
 
     match word >> 8 {
+        0x18 | 0x19 => binary_register_3op::<op::Add>(core, pc, word),
+        //0x1a | 0x1b => binary_register_3op::<op::Sub, false>(core, pc, word),
+        0x1c | 0x1d => binary_immediate_3op::<op::Add>(core, pc, word),
+        //0x1e | 0x1f => binary__immediate_3op::<op::Sub>(core, pc, word),
         0x20..=0x27 => move_immediate(core, pc, word),
         0x28..=0x2f => compare_immediate(core, pc, word),
         0x30..=0x37 => binary_immediate::<op::Add>(core, pc, word),
--- utopia/src/core/arm7tdmi/thumb/process.rs
+++ utopia/src/core/arm7tdmi/thumb/process.rs
@@ -2,6 +2,42 @@ use super::super::operator::{BinaryOperator, Cmp, ComparisonOperator, Mov, MoveO
 use super::super::{Bus, Core, REGS};
 use tracing::debug;
 
+pub fn binary_register_3op<Op: BinaryOperator>(core: &mut Core<impl Bus>, pc: u32, word: u16) {
+    let rn = ((word >> 6) & 7) as usize;
+    let rs = ((word >> 3) & 7) as usize;
+    let rd = (word & 7) as usize;
+
+    debug!(
+        "{:08X} {} {}, {}, {}",
+        pc,
+        Op::NAME,
+        REGS[rd],
+        REGS[rs],
+        REGS[rn]
+    );
+
+    let result = Op::apply::<true>(core, core.get(rs), core.get(rn));
+    core.set(rd, result);
+}
+
+pub fn binary_immediate_3op<Op: BinaryOperator>(core: &mut Core<impl Bus>, pc: u32, word: u16) {
+    let value = (word >> 6) & 7;
+    let rs = ((word >> 3) & 7) as usize;
+    let rd = (word & 7) as usize;
+
+    debug!(
+        "{:08X} {} {}, {}, #{}",
+        pc,
+        Op::NAME,
+        REGS[rd],
+        REGS[rs],
+        value
+    );
+
+    let result = Op::apply::<true>(core, core.get(rs), value as u32);
+    core.set(rd, result);
+}
+
 pub fn binary_immediate<Op: BinaryOperator>(core: &mut Core<impl Bus>, pc: u32, word: u16) {
     let rd = ((word >> 8) & 7) as usize;
     let value = word & 0xff;
