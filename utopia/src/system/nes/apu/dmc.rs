d83c71a87f2214fbdcb83ea9d7a41bacf1feab2d
James Lippitt <james.lippitt@gmail.com>
2023-08-02 14:38:18 +0100

    Clean up DMC code

--- utopia/src/system/nes/apu/dmc.rs
+++ utopia/src/system/nes/apu/dmc.rs
@@ -8,15 +8,23 @@ const PERIODS: [u32; 16] = [
     189, 159, 141, 127, 105,  83,  71,  53,
 ];
 
+struct Shifter {
+    value: u8,
+    bits_remaining: u8,
+}
+
+struct Reader {
+    address: u16,
+    bytes_remaining: u16,
+}
+
 pub struct Dmc {
     output: u8,
     timer: Timer,
     silence_flag: bool,
-    shift: u8,
-    bits_remaining: u8,
+    shifter: Shifter,
     sample_buffer: Option<u8>,
-    current_address: u16,
-    bytes_remaining: u16,
+    reader: Reader,
     sample_address: u16,
     sample_length: u16,
     loop_flag: bool,
@@ -28,11 +36,15 @@ impl Dmc {
             output: 0,
             timer: Timer::new(PERIODS[0], 0),
             silence_flag: false,
-            shift: 0,
-            bits_remaining: 0,
+            shifter: Shifter {
+                value: 0,
+                bits_remaining: 0,
+            },
             sample_buffer: None,
-            current_address: 0,
-            bytes_remaining: 0,
+            reader: Reader {
+                address: 0xc000,
+                bytes_remaining: 0,
+            },
             sample_address: 0,
             sample_length: 0,
             loop_flag: false,
@@ -40,7 +52,7 @@ impl Dmc {
     }
 
     pub fn enabled(&self) -> bool {
-        self.bytes_remaining > 0
+        self.reader.bytes_remaining > 0
     }
 
     pub fn output(&self) -> u8 {
@@ -48,7 +60,7 @@ impl Dmc {
     }
 
     pub fn sample_address(&self) -> u16 {
-        self.current_address
+        self.reader.address
     }
 
     pub fn write(&mut self, address: u16, value: u8) {
@@ -67,27 +79,27 @@ impl Dmc {
 
     pub fn set_enabled(&mut self, enabled: bool) {
         if enabled {
-            if self.bytes_remaining == 0 {
+            if self.reader.bytes_remaining == 0 {
                 self.restart();
             }
         } else {
-            self.bytes_remaining = 0;
+            self.reader.bytes_remaining = 0;
         }
     }
 
     pub fn write_sample(&mut self, value: u8) {
         self.sample_buffer = Some(value);
-        self.current_address = 0x8000 | (self.current_address.wrapping_add(1) & 0x7fff);
+        self.reader.address = 0x8000 | (self.reader.address.wrapping_add(1) & 0x7fff);
 
         // This means sample length is one greater than what is written to register $4013
-        if self.bytes_remaining == 0 {
+        if self.reader.bytes_remaining == 0 {
             if self.loop_flag {
                 self.restart();
             } else {
                 // TODO: IRQ
             }
         } else {
-            self.bytes_remaining -= 1;
+            self.reader.bytes_remaining -= 1;
         }
     }
 
@@ -97,7 +109,7 @@ impl Dmc {
         }
 
         if !self.silence_flag {
-            if (self.shift & 1) != 0 {
+            if (self.shifter.value & 1) != 0 {
                 if self.output <= 125 {
                     self.output += 2;
                 }
@@ -106,36 +118,36 @@ impl Dmc {
             }
         }
 
-        self.shift >>= 1;
+        self.shifter.value >>= 1;
 
-        if self.bits_remaining == 0 {
+        if self.shifter.bits_remaining == 0 {
             // One less than total bits in sample, due to way this is implemented
-            self.bits_remaining = 7;
+            self.shifter.bits_remaining = 7;
 
             if let Some(sample) = self.sample_buffer {
-                self.shift = sample;
+                self.shifter.value = sample;
                 self.sample_buffer = None;
                 self.silence_flag = false;
             } else {
                 self.silence_flag = true;
             }
 
-            if self.bytes_remaining != 0 {
+            if self.reader.bytes_remaining != 0 {
                 dma_request.insert(DmaRequest::DMC);
             }
         } else {
-            self.bits_remaining -= 1;
+            self.shifter.bits_remaining -= 1;
         }
     }
 
     fn restart(&mut self) {
-        self.current_address = self.sample_address;
-        self.bytes_remaining = self.sample_length;
+        self.reader.address = self.sample_address;
+        self.reader.bytes_remaining = self.sample_length;
 
         debug!(
             "DMC Restart: Address = {:04X}, Length = {}",
-            self.current_address,
-            self.bytes_remaining + 1
+            self.reader.address,
+            self.reader.bytes_remaining + 1
         );
     }
 }
