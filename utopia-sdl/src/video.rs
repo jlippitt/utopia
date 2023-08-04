e80f691980d1736a74940d96c935f25af48ebdda
James Lippitt <james.lippitt@gmail.com>
2023-08-04 17:20:28 +0100

    Factor out video scaling logic into 'display mode' module

--- utopia-sdl/src/video.rs
+++ utopia-sdl/src/video.rs
@@ -1,11 +1,13 @@
+use display_mode::DisplayMode;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
 use sdl2::render::{Canvas, Texture, TextureCreator, TextureValueError};
 use sdl2::video::{Window, WindowContext};
 use sdl2::Sdl;
 use sdl2::VideoSubsystem;
-use std::cmp;
 use std::error::Error;
+
+mod display_mode;
 
 pub struct VideoOptions {
     pub width: u32,
@@ -25,8 +27,8 @@ pub struct Video {
     canvas: Canvas<Window>,
     source_rect: Rect,
     target_rect: Rect,
+    display_mode: DisplayMode,
     full_screen: bool,
-    upscale: Option<u32>,
 }
 
 impl Video {
@@ -37,13 +39,10 @@ impl Video {
 
         let clipped_height = options.height - options.clip_top - options.clip_bottom;
 
-        let (window_width, window_height) = calculate_window_size(
-            &video,
-            options.width,
-            clipped_height,
-            options.full_screen,
-            options.upscale,
-        )?;
+        let display_mode = DisplayMode::new(options.width, clipped_height, options.upscale);
+
+        let (window_width, window_height) =
+            display_mode.window_size(&video, options.full_screen)?;
 
         let mut window_builder = video.window("Utopia", window_width, window_height);
 
@@ -70,13 +69,7 @@ impl Video {
             clipped_height,
         );
 
-        let target_rect = calculate_target_rect(
-            &video,
-            options.width,
-            clipped_height,
-            options.full_screen,
-            options.upscale,
-        )?;
+        let target_rect = display_mode.target_rect(&video, options.full_screen)?;
 
         Ok(Self {
             video,
@@ -86,8 +79,8 @@ impl Video {
             canvas,
             source_rect,
             target_rect,
+            display_mode,
             full_screen: options.full_screen,
-            upscale: options.upscale,
         })
     }
 
@@ -103,13 +96,9 @@ impl Video {
     }
 
     pub fn on_size_changed(&mut self) -> Result<(), Box<dyn Error>> {
-        self.target_rect = calculate_target_rect(
-            &self.video,
-            self.source_rect.width(),
-            self.source_rect.height(),
-            self.full_screen,
-            self.upscale,
-        )?;
+        self.target_rect = self
+            .display_mode
+            .target_rect(&self.video, self.full_screen)?;
 
         Ok(())
     }
@@ -128,85 +117,4 @@ impl Video {
 
         Ok(())
     }
-}
-
-fn calculate_window_size(
-    video: &VideoSubsystem,
-    width: u32,
-    height: u32,
-    full_screen: bool,
-    upscale: Option<u32>,
-) -> Result<(u32, u32), Box<dyn Error>> {
-    if !full_screen {
-        let rect = calculate_target_rect(video, width, height, full_screen, upscale)?;
-        return Ok((rect.width(), rect.height()));
-    }
-
-    let mut max_scale: u32 = 0;
-
-    for index in 0..video.num_display_modes(0)? {
-        let display_mode = video.display_mode(0, index)?;
-
-        if display_mode.format.byte_size_per_pixel() < 3 {
-            // Needs at least 24-bit colour to be considered
-            continue;
-        }
-
-        let width_ratio = display_mode.w as u32 / width;
-        let height_ratio = display_mode.h as u32 / height;
-        let scale = cmp::min(width_ratio, height_ratio);
-
-        if scale <= max_scale {
-            continue;
-        }
-
-        max_scale = scale;
-    }
-
-    Ok(apply_upscale(max_scale, upscale, width, height))
-}
-
-fn calculate_target_rect(
-    video: &VideoSubsystem,
-    width: u32,
-    height: u32,
-    full_screen: bool,
-    upscale: Option<u32>,
-) -> Result<Rect, Box<dyn Error>> {
-    let display_bounds = if full_screen {
-        video.display_bounds(0)?
-    } else {
-        video.display_usable_bounds(0)?
-    };
-
-    let width_ratio = display_bounds.width() / width;
-    let height_ratio = display_bounds.height() / height;
-    let max_scale = cmp::min(width_ratio, height_ratio);
-
-    let (scaled_width, scaled_height) = apply_upscale(max_scale, upscale, width, height);
-
-    let (offset_x, offset_y) = if full_screen {
-        (
-            (display_bounds.width() - scaled_width) >> 1,
-            (display_bounds.height() - scaled_height) >> 1,
-        )
-    } else {
-        (0, 0)
-    };
-
-    let rect = Rect::new(
-        offset_x as i32,
-        offset_y as i32,
-        scaled_width,
-        scaled_height,
-    );
-
-    return Ok(rect);
-}
-
-fn apply_upscale(max_scale: u32, upscale: Option<u32>, width: u32, height: u32) -> (u32, u32) {
-    let scale = upscale.map_or(max_scale, |scale| cmp::min(scale, max_scale));
-    let scaled_width = width * scale;
-    let scaled_height = height * scale;
-    (scaled_width, scaled_height)
 }
---
+++ utopia-sdl/src/video/display_mode.rs
@@ -0,0 +1,99 @@
+use sdl2::rect::Rect;
+use sdl2::VideoSubsystem;
+use std::cmp;
+use std::error::Error;
+
+pub struct DisplayMode {
+    width: u32,
+    height: u32,
+    upscale: Option<u32>,
+}
+
+impl DisplayMode {
+    pub fn new(width: u32, height: u32, upscale: Option<u32>) -> Self {
+        Self {
+            width,
+            height,
+            upscale,
+        }
+    }
+
+    pub fn window_size(
+        &self,
+        video: &VideoSubsystem,
+        full_screen: bool,
+    ) -> Result<(u32, u32), Box<dyn Error>> {
+        if !full_screen {
+            let rect = self.target_rect(video, full_screen)?;
+            return Ok((rect.width(), rect.height()));
+        }
+
+        let mut max_scale: u32 = 0;
+
+        for index in 0..video.num_display_modes(0)? {
+            let display_mode = video.display_mode(0, index)?;
+
+            if display_mode.format.byte_size_per_pixel() < 3 {
+                // Needs at least 24-bit colour to be considered
+                continue;
+            }
+
+            let width_ratio = display_mode.w as u32 / self.width;
+            let height_ratio = display_mode.h as u32 / self.height;
+            let scale = cmp::min(width_ratio, height_ratio);
+
+            if scale <= max_scale {
+                continue;
+            }
+
+            max_scale = scale;
+        }
+
+        Ok(self.apply_upscale(max_scale))
+    }
+
+    pub fn target_rect(
+        &self,
+        video: &VideoSubsystem,
+        full_screen: bool,
+    ) -> Result<Rect, Box<dyn Error>> {
+        let display_bounds = if full_screen {
+            video.display_bounds(0)?
+        } else {
+            video.display_usable_bounds(0)?
+        };
+
+        let width_ratio = display_bounds.width() / self.width;
+        let height_ratio = display_bounds.height() / self.height;
+        let max_scale = cmp::min(width_ratio, height_ratio);
+
+        let (scaled_width, scaled_height) = self.apply_upscale(max_scale);
+
+        let (offset_x, offset_y) = if full_screen {
+            (
+                (display_bounds.width() - scaled_width) >> 1,
+                (display_bounds.height() - scaled_height) >> 1,
+            )
+        } else {
+            (0, 0)
+        };
+
+        let rect = Rect::new(
+            offset_x as i32,
+            offset_y as i32,
+            scaled_width,
+            scaled_height,
+        );
+
+        return Ok(rect);
+    }
+
+    fn apply_upscale(&self, max_scale: u32) -> (u32, u32) {
+        let scale = self
+            .upscale
+            .map_or(max_scale, |scale| cmp::min(scale, max_scale));
+        let scaled_width = self.width * scale;
+        let scaled_height = self.height * scale;
+        (scaled_width, scaled_height)
+    }
+}
