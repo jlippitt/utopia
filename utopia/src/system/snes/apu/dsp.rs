295c0aeaea7012ecd319934949aaee53d8d18935
James Lippitt <james.lippitt@gmail.com>
2023-08-05 17:14:07 +0100

    Map DSP registers to their intended locations

--- utopia/src/system/snes/apu/dsp.rs
+++ utopia/src/system/snes/apu/dsp.rs
@@ -1,9 +1,13 @@
 use tracing::debug;
+use voice::Voice;
+
+mod voice;
 
 const TOTAL_REGISTERS: usize = 128;
 
 pub struct Dsp {
     address: u8,
+    voices: [Voice; 8],
     data: [u8; TOTAL_REGISTERS],
 }
 
@@ -11,6 +15,16 @@ impl Dsp {
     pub fn new() -> Self {
         Self {
             address: 0,
+            voices: [
+                Voice::new(0),
+                Voice::new(1),
+                Voice::new(2),
+                Voice::new(3),
+                Voice::new(4),
+                Voice::new(5),
+                Voice::new(6),
+                Voice::new(7),
+            ],
             data: [0; TOTAL_REGISTERS],
         }
     }
@@ -25,7 +39,24 @@ impl Dsp {
     }
 
     pub fn read(&self) -> u8 {
-        self.data[self.address as usize & 0x7f]
+        let address = self.address & 0x7f;
+
+        let value = match self.address & 0x0f {
+            0x08 => self.voice(address).envelope(),
+            0x09 => self.voice(address).output(),
+            _ => {
+                if address == 0x7c {
+                    // TODO: ENDX
+                    0
+                } else {
+                    self.data[address as usize]
+                }
+            }
+        };
+
+        debug!("DSP Read: {:02X} >= {:02X}", address, value);
+
+        value
     }
 
     pub fn write(&mut self, value: u8) {
@@ -35,5 +66,56 @@ impl Dsp {
 
         self.data[self.address as usize] = value;
         debug!("DSP Write: {:02X} <= {:02X}", self.address, value);
+
+        match self.address & 0x0f {
+            0x00 => self.voice_mut(self.address).set_volume_left(value),
+            0x01 => self.voice_mut(self.address).set_volume_right(value),
+            0x02 => self.voice_mut(self.address).set_pitch_low(value),
+            0x03 => self.voice_mut(self.address).set_pitch_high(value),
+            0x04 => self.voice_mut(self.address).set_source(value),
+            0x05 => self.voice_mut(self.address).set_adsr_low(value),
+            0x06 => self.voice_mut(self.address).set_adsr_high(value),
+            0x07 => self.voice_mut(self.address).set_gain(value),
+            0x08 => (), // TODO: ENVX (read-only?)
+            0x09 => (), // TODO: OUTX (read-only?)
+            0x0c => {
+                match self.address {
+                    0x0c => (), // TODO: Main volume (left)
+                    0x1c => (), // TODO: Main volume (right)
+                    0x2c => (), // TODO: Echo volume (left)
+                    0x3c => (), // TODO: Echo volume (right)
+                    0x4c => (), // TODO: Key on
+                    0x5c => (), // TODO: Key off
+                    0x6c => (), // TODO: Flags
+                    0x7c => (), // TODO: ENDX (read-only?)
+                    _ => unreachable!(),
+                }
+            }
+            0x0d => {
+                match self.address {
+                    0x0d => (), // TODO: Echo feedback
+                    0x1d => (), // TODO: Not used
+                    0x2d => (), // TODO: Pitch modulation
+                    0x3d => (), // TODO: Noise enable
+                    0x4d => (), // TOOD: Echo enable
+                    0x5d => (), // TODO: Directory
+                    0x6d => (), // TODO: Echo start address
+                    0x7d => (), // TODO: Echo delay
+                    _ => unreachable!(),
+                }
+            }
+            0x0f => {
+                // TODO: FIR coefficients
+            }
+            _ => (), // TODO: Not used
+        }
+    }
+
+    fn voice(&self, address: u8) -> &Voice {
+        &self.voices[(address >> 4) as usize]
+    }
+
+    fn voice_mut(&mut self, address: u8) -> &mut Voice {
+        &mut self.voices[(address >> 4) as usize]
     }
 }
---
+++ utopia/src/system/snes/apu/dsp/voice.rs
@@ -0,0 +1,51 @@
+pub struct Voice {
+    _id: u32,
+}
+
+impl Voice {
+    pub fn new(id: u32) -> Self {
+        Self { _id: id }
+    }
+
+    pub fn envelope(&self) -> u8 {
+        // TODO
+        0
+    }
+
+    pub fn output(&self) -> u8 {
+        // TODO
+        0
+    }
+
+    pub fn set_volume_left(&mut self, _value: u8) {
+        // TODO
+    }
+
+    pub fn set_volume_right(&mut self, _value: u8) {
+        // TODO
+    }
+
+    pub fn set_pitch_low(&mut self, _value: u8) {
+        // TODO
+    }
+
+    pub fn set_pitch_high(&mut self, _value: u8) {
+        // TODO
+    }
+
+    pub fn set_source(&mut self, _value: u8) {
+        // TODO
+    }
+
+    pub fn set_adsr_low(&mut self, _value: u8) {
+        // TODO
+    }
+
+    pub fn set_adsr_high(&mut self, _value: u8) {
+        // TODO
+    }
+
+    pub fn set_gain(&mut self, _value: u8) {
+        // TODO
+    }
+}
