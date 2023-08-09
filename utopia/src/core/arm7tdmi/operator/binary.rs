d24a0ff6d64a0199ba33dcaffed11efc86796d71
James Lippitt <james.lippitt@gmail.com>
2023-08-09 11:28:45 +0100

    Implement binary logic ops

--- utopia/src/core/arm7tdmi/operator/binary.rs
+++ utopia/src/core/arm7tdmi/operator/binary.rs
@@ -24,3 +24,67 @@ impl BinaryOperator for Adc {
         core.add_with_carry::<SET_FLAGS>(lhs, rhs, true)
     }
 }
+
+pub struct And;
+
+impl BinaryOperator for And {
+    const NAME: &'static str = "AND";
+
+    fn apply<const SET_FLAGS: bool>(core: &mut Core<impl Bus>, lhs: u32, rhs: u32) -> u32 {
+        let result = lhs & rhs;
+
+        if SET_FLAGS {
+            core.set_nz(result);
+        }
+
+        result
+    }
+}
+
+pub struct Eor;
+
+impl BinaryOperator for Eor {
+    const NAME: &'static str = "EOR";
+
+    fn apply<const SET_FLAGS: bool>(core: &mut Core<impl Bus>, lhs: u32, rhs: u32) -> u32 {
+        let result = lhs ^ rhs;
+
+        if SET_FLAGS {
+            core.set_nz(result);
+        }
+
+        result
+    }
+}
+
+pub struct Orr;
+
+impl BinaryOperator for Orr {
+    const NAME: &'static str = "ORR";
+
+    fn apply<const SET_FLAGS: bool>(core: &mut Core<impl Bus>, lhs: u32, rhs: u32) -> u32 {
+        let result = lhs | rhs;
+
+        if SET_FLAGS {
+            core.set_nz(result);
+        }
+
+        result
+    }
+}
+
+pub struct Bic;
+
+impl BinaryOperator for Bic {
+    const NAME: &'static str = "BIC";
+
+    fn apply<const SET_FLAGS: bool>(core: &mut Core<impl Bus>, lhs: u32, rhs: u32) -> u32 {
+        let result = lhs & !rhs;
+
+        if SET_FLAGS {
+            core.set_nz(result);
+        }
+
+        result
+    }
+}
--- utopia/src/core/arm7tdmi/thumb/process.rs
+++ utopia/src/core/arm7tdmi/thumb/process.rs
@@ -122,21 +122,21 @@ pub fn alu_operation(core: &mut Core<impl Bus>, pc: u32, word: u16) {
     let rd = (word & 7) as usize;
 
     match (word >> 6) & 15 {
-        // 0b0000 => binary_op::<op::And>(core, pc, rs, rd),
-        // 0b0001 => binary_op::<op::Eor>(core, pc, rs, rd),
+        0b0000 => binary_op::<op::And>(core, pc, rs, rd),
+        0b0001 => binary_op::<op::Eor>(core, pc, rs, rd),
         // 0b0010 => shift_op::<op::Lsl>(core, pc, rs, rd),
         // 0b0011 => shift_op::<op::Lsr>(core, pc, rs, rd),
         // 0b0100 => shift_op::<op::Asr>(core, pc, rs, rd),
-        // 0b0101 => binary_op::<op::Adc>(core, pc, rs, rd),
+        0b0101 => binary_op::<op::Adc>(core, pc, rs, rd),
         // 0b0110 => binary_op::<op::Sbc>(core, pc, rs, rd),
         // 0b0111 => shift_op::<op::Ror>(core, pc, rs, rd),
         0b1000 => compare_op::<op::Tst>(core, pc, rs, rd),
         // 0b1001 => move_op::<op::Neg>(core, pc, rs, rd),
         0b1010 => compare_op::<op::Cmp>(core, pc, rs, rd),
         0b1011 => compare_op::<op::Cmn>(core, pc, rs, rd),
-        // 0b1100 => binary_op::<op::Orr>(core, pc, rs, rd),
+        0b1100 => binary_op::<op::Orr>(core, pc, rs, rd),
         // 0b1101 => binary_op::<op::Mul>(core, pc, rs, rd),
-        // 0b1110 => binary_op::<op::Bic>(core, pc, rs, rd),
+        0b1110 => binary_op::<op::Bic>(core, pc, rs, rd),
         0b1111 => move_op::<op::Mvn>(core, pc, rs, rd),
         opcode => todo!("ALU operation {:04b}", opcode),
         //_ => unreachable!(),
@@ -152,4 +152,10 @@ fn move_op<Op: MoveOperator>(core: &mut Core<impl Bus>, pc: u32, rs: usize, rd: 
 fn compare_op<Op: CompareOperator>(core: &mut Core<impl Bus>, pc: u32, rs: usize, rd: usize) {
     debug!("{:08X} {} {}, {}", pc, Op::NAME, REGS[rd], REGS[rs]);
     Op::apply(core, core.get(rd), core.get(rs));
+}
+
+fn binary_op<Op: BinaryOperator>(core: &mut Core<impl Bus>, pc: u32, rs: usize, rd: usize) {
+    debug!("{:08X} {} {}, {}", pc, Op::NAME, REGS[rd], REGS[rs]);
+    let result = Op::apply::<true>(core, core.get(rd), core.get(rs));
+    core.set(rd, result);
 }
