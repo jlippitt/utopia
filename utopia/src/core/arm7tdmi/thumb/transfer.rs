545c1f90423a61040e1032f181f7f020976245b3
James Lippitt <james.lippitt@gmail.com>
2023-08-10 10:56:15 +0100

    Implement thumb loads/stores for bytes/words

--- utopia/src/core/arm7tdmi/thumb/transfer.rs
+++ utopia/src/core/arm7tdmi/thumb/transfer.rs
@@ -20,6 +20,93 @@ fn reg_list(word: u16, extra: Option<usize>) -> String {
     reg_list.join(", ")
 }
 
+pub fn ldr_byte(core: &mut Core<impl Bus>, pc: u32, word: u16) {
+    let offset = (word >> 6) & 31;
+    let rb = ((word >> 3) & 7) as usize;
+    let rd = (word & 7) as usize;
+
+    debug!(
+        "{:08X} LDRB {}, [{}, #0x{:X}]",
+        pc, REGS[rd], REGS[rb], offset
+    );
+
+    let address = core.get(rb).wrapping_add(offset as u32);
+    let result = core.read_byte(address);
+    core.set(rd, result as u32);
+}
+
+pub fn ldr_halfword(core: &mut Core<impl Bus>, pc: u32, word: u16) {
+    let offset = ((word >> 6) & 31) << 1;
+    let rb = ((word >> 3) & 7) as usize;
+    let rd = (word & 7) as usize;
+
+    debug!(
+        "{:08X} LDRH {}, [{}, #0x{:X}]",
+        pc, REGS[rd], REGS[rb], offset
+    );
+
+    let address = core.get(rb).wrapping_add(offset as u32);
+    let result = core.read_halfword(address);
+    core.set(rd, result as u32);
+}
+
+pub fn ldr_word(core: &mut Core<impl Bus>, pc: u32, word: u16) {
+    let offset = ((word >> 6) & 31) << 2;
+    let rb = ((word >> 3) & 7) as usize;
+    let rd = (word & 7) as usize;
+
+    debug!(
+        "{:08X} LDR {}, [{}, #0x{:X}]",
+        pc, REGS[rd], REGS[rb], offset
+    );
+
+    let address = core.get(rb).wrapping_add(offset as u32);
+    let result = core.read_word(address);
+    core.set(rd, result);
+}
+
+pub fn str_byte(core: &mut Core<impl Bus>, pc: u32, word: u16) {
+    let offset = (word >> 6) & 31;
+    let rb = ((word >> 3) & 7) as usize;
+    let rd = (word & 7) as usize;
+
+    debug!(
+        "{:08X} STRB {}, [{}, #0x{:X}]",
+        pc, REGS[rd], REGS[rb], offset
+    );
+
+    let address = core.get(rb).wrapping_add(offset as u32);
+    core.write_byte(address, core.get(rd) as u8);
+}
+
+pub fn str_halfword(core: &mut Core<impl Bus>, pc: u32, word: u16) {
+    let offset = ((word >> 6) & 31) << 1;
+    let rb = ((word >> 3) & 7) as usize;
+    let rd = (word & 7) as usize;
+
+    debug!(
+        "{:08X} STRH {}, [{}, #0x{:X}]",
+        pc, REGS[rd], REGS[rb], offset
+    );
+
+    let address = core.get(rb).wrapping_add(offset as u32);
+    core.write_halfword(address, core.get(rd) as u16);
+}
+
+pub fn str_word(core: &mut Core<impl Bus>, pc: u32, word: u16) {
+    let offset = ((word >> 6) & 31) << 2;
+    let rb = ((word >> 3) & 7) as usize;
+    let rd = (word & 7) as usize;
+
+    debug!(
+        "{:08X} STR {}, [{}, #0x{:X}]",
+        pc, REGS[rd], REGS[rb], offset
+    );
+
+    let address = core.get(rb).wrapping_add(offset as u32);
+    core.write_word(address, core.get(rd));
+}
+
 pub fn ldr_register<const BYTE: bool>(core: &mut Core<impl Bus>, pc: u32, word: u16) {
     let ro = ((word >> 6) & 7) as usize;
     let rb = ((word >> 3) & 7) as usize;
@@ -67,36 +154,6 @@ pub fn str_register<const BYTE: bool>(core: &mut Core<impl Bus>, pc: u32, word: 
         core.write_word(address, core.get(rd));
     }
 }
-
-pub fn ldr_halfword(core: &mut Core<impl Bus>, pc: u32, word: u16) {
-    let offset = ((word >> 6) & 31) << 1;
-    let rb = ((word >> 3) & 7) as usize;
-    let rd = (word & 7) as usize;
-
-    debug!(
-        "{:08X} LDRH {}, [{}, #0x{:X}]",
-        pc, REGS[rd], REGS[rb], offset
-    );
-
-    let address = core.get(rb).wrapping_add(offset as u32);
-    let result = core.read_halfword(address);
-    core.set(rd, result as u32);
-}
-
-pub fn str_halfword(core: &mut Core<impl Bus>, pc: u32, word: u16) {
-    let offset = ((word >> 6) & 31) << 1;
-    let rb = ((word >> 3) & 7) as usize;
-    let rd = (word & 7) as usize;
-
-    debug!(
-        "{:08X} STRH {}, [{}, #0x{:X}]",
-        pc, REGS[rd], REGS[rb], offset
-    );
-
-    let address = core.get(rb).wrapping_add(offset as u32);
-    core.write_halfword(address, core.get(rd) as u16);
-}
-
 pub fn ldr_pc_relative(core: &mut Core<impl Bus>, pc: u32, word: u16) {
     let rd = ((word >> 8) & 7) as usize;
     let offset = (word & 0xff) << 2;
