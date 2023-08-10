0e72e51951ae079e29b2a8207749e482c6f9e700
James Lippitt <james.lippitt@gmail.com>
2023-08-10 08:48:04 +0100

    Make comparisons work for registers as well

--- utopia/src/core/arm7tdmi/arm/process.rs
+++ utopia/src/core/arm7tdmi/arm/process.rs
@@ -151,6 +151,33 @@ pub fn move_register<Op: MoveOperator, const SET_FLAGS: bool>(
     core.set(rd, result);
 }
 
+pub fn compare_register<Op: CompareOperator>(core: &mut Core<impl Bus>, pc: u32, word: u32) {
+    let rn = ((word >> 16) & 15) as usize;
+    let rm = (word & 15) as usize;
+    let shift_type = ((word >> 5) & 3) as usize;
+    let shift_amount = (word >> 7) & 31;
+
+    debug!(
+        "{:08X} {} {}, {}, {} #0x{:X}",
+        pc,
+        Op::NAME,
+        REGS[rn],
+        REGS[rm],
+        SHIFT[shift_type],
+        shift_amount,
+    );
+
+    let value = match shift_type {
+        0b00 => Lsl::apply::<true>(core, core.get(rm), shift_amount),
+        0b01 => Lsr::apply::<true>(core, core.get(rm), shift_amount),
+        0b10 => todo!("ASR"),
+        0b11 => todo!("ROR"),
+        _ => unreachable!(),
+    };
+
+    Op::apply(core, core.get(rn), value);
+}
+
 pub fn msr_register<const SPSR: bool>(core: &mut Core<impl Bus>, pc: u32, word: u32) {
     let rm = (word & 15) as usize;
     let control = (word & 0x0001_0000) != 0;
