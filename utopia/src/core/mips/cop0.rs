c787fdaae03106276968fb8be1f425197328377b
James Lippitt <james.lippitt@gmail.com>
2023-08-13 13:49:16 +0100

    Stopgap TLBWI implementation

--- utopia/src/core/mips/cop0.rs
+++ utopia/src/core/mips/cop0.rs
@@ -1,7 +1,7 @@
 use super::{Bus, Core, REGS};
 use tracing::debug;
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct TlbEntry {
     lo0: u32,
     lo1: u32,
@@ -10,7 +10,10 @@ pub struct TlbEntry {
 
 #[derive(Default)]
 pub struct Cop0 {
-    index: usize,
+    index: u32,
+    lo0: u32,
+    lo1: u32,
+    hi: u32,
     tlb_entries: [TlbEntry; 32],
 }
 
@@ -18,6 +21,15 @@ pub fn dispatch(core: &mut Core<impl Bus>, word: u32) {
     match (word >> 21) & 31 {
         0b00000 => type_r(core, mfc0, word),
         0b00100 => type_r(core, mtc0, word),
+        0b10000..=0b11111 => match word & 63 {
+            0b000010 => tlbwi(core),
+            func => unimplemented!(
+                "COP0 RS=10000 FN={:06b} ({:08X}: {:08X})",
+                func,
+                core.pc,
+                word
+            ),
+        },
         rs => unimplemented!("COP0 RS={:05b} ({:08X}: {:08X})", rs, core.pc, word),
     }
 }
@@ -32,9 +44,10 @@ fn mfc0(core: &mut Core<impl Bus>, rt: usize, rd: usize) {
     debug!("{:08X} MFC0 {}, ${}", core.pc, REGS[rt], rd);
 
     let result = match rd {
-        2 => core.cop0.tlb_entries[core.cop0.index].lo0,
-        3 => core.cop0.tlb_entries[core.cop0.index].lo1,
-        10 => core.cop0.tlb_entries[core.cop0.index].hi,
+        0 => core.cop0.index,
+        2 => core.cop0.lo0,
+        3 => core.cop0.lo1,
+        10 => core.cop0.hi,
         12 => {
             // STATUS
             // TODO: Interrupts/Exceptions
@@ -53,20 +66,20 @@ fn mtc0(core: &mut Core<impl Bus>, rt: usize, rd: usize) {
 
     match rd {
         0 => {
-            core.cop0.index = value as usize & 31;
-            debug!("COP0 Index: {}", core.cop0.index);
+            core.cop0.index = value;
+            debug!("  COP0 Index: {}", core.cop0.index);
         }
         2 => {
-            core.cop0.tlb_entries[core.cop0.index].lo0 = value;
-            debug!("COP0 TLB[{}] LO0: {:08X}", core.cop0.index, value);
+            core.cop0.lo0 = value;
+            debug!("  COP0 LO0: {:08X}", value);
         }
         3 => {
-            core.cop0.tlb_entries[core.cop0.index].lo1 = value;
-            debug!("COP0 TLB[{}] LO1: {:08X}", core.cop0.index, value);
+            core.cop0.lo1 = value;
+            debug!("  COP0 LO1: {:08X}", value);
         }
         10 => {
-            core.cop0.tlb_entries[core.cop0.index].hi = value;
-            debug!("COP0 TLB[{}] HI: {:08X}", core.cop0.index, value);
+            core.cop0.hi = value;
+            debug!("  COP0 HI: {:08X}", value);
         }
         12 => {
             // STATUS
@@ -81,4 +94,15 @@ fn mtc0(core: &mut Core<impl Bus>, rt: usize, rd: usize) {
             }
         }
     }
+}
+
+fn tlbwi(core: &mut Core<impl Bus>) {
+    debug!("{:08X} TLBWI", core.pc);
+
+    let tlb_entry = &mut core.cop0.tlb_entries[core.cop0.index as usize & 31];
+    tlb_entry.lo0 = core.cop0.lo0;
+    tlb_entry.lo1 = core.cop0.lo1;
+    tlb_entry.hi = core.cop0.hi;
+
+    debug!("TLB Entry {}: {:X?}", core.cop0.index, tlb_entry);
 }
