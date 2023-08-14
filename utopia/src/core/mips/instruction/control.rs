0b8179ba63a7da86c48f745ab686a712f1c30433
James Lippitt <james.lippitt@gmail.com>
2023-08-14 10:14:18 +0100

    Set link registers to 64-bit value of PC

--- utopia/src/core/mips/instruction/control.rs
+++ utopia/src/core/mips/instruction/control.rs
@@ -11,7 +11,7 @@ pub fn j(core: &mut Core<impl Bus>, value: u32) {
 pub fn jal(core: &mut Core<impl Bus>, value: u32) {
     let target = (core.next[0] & 0xfc00_0000) | (value << 2);
     debug!("{:08X} JAL 0x{:08X}", core.pc, target);
-    core.set(31, core.next[1]);
+    core.setd(31, core.next[1] as u64);
     core.next[1] = target;
 }
 
@@ -22,7 +22,7 @@ pub fn jr(core: &mut Core<impl Bus>, rs: usize, _rt: usize, _rd: usize, _sa: u32
 
 pub fn jalr(core: &mut Core<impl Bus>, rs: usize, _rt: usize, _rd: usize, _sa: u32) {
     debug!("{:08X} JALR {}", core.pc, REGS[rs]);
-    core.set(31, core.next[1]);
+    core.setd(31, core.next[1] as u64);
     core.next[1] = core.get(rs);
 }
 
