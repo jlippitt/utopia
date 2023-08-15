f1c8fb6863a7076abdac2446dd4819636fd34d23
James Lippitt <james.lippitt@gmail.com>
2023-08-15 08:59:48 +0100

    Implement missing 64-bit shifts

--- utopia/src/core/mips/instruction/register64.rs
+++ utopia/src/core/mips/instruction/register64.rs
@@ -13,6 +13,18 @@ pub fn dsll32(core: &mut Core<impl Bus>, _rs: usize, rt: usize, rd: usize, sa: u
     core.setd(rd, result);
 }
 
+pub fn dsrl(core: &mut Core<impl Bus>, _rs: usize, rt: usize, rd: usize, sa: u32) {
+    debug!("{:08X} DSRL {}, {}, {}", core.pc, REGS[rd], REGS[rt], sa);
+    let result = core.getd(rt) >> sa;
+    core.setd(rd, result);
+}
+
+pub fn dsrl32(core: &mut Core<impl Bus>, _rs: usize, rt: usize, rd: usize, sa: u32) {
+    debug!("{:08X} DSRL32 {}, {}, {}", core.pc, REGS[rd], REGS[rt], sa);
+    let result = core.getd(rt) >> (sa + 32);
+    core.setd(rd, result);
+}
+
 pub fn dsra(core: &mut Core<impl Bus>, _rs: usize, rt: usize, rd: usize, sa: u32) {
     debug!("{:08X} DSRA {}, {}, {}", core.pc, REGS[rd], REGS[rt], sa);
     let result = (core.getd(rt) as i64 >> sa) as u64;
@@ -31,6 +43,24 @@ pub fn dsllv(core: &mut Core<impl Bus>, rs: usize, rt: usize, rd: usize, _sa: u3
         core.pc, REGS[rd], REGS[rt], REGS[rs]
     );
     let result = core.getd(rt) << (core.getd(rs) & 63);
+    core.setd(rd, result);
+}
+
+pub fn dsrlv(core: &mut Core<impl Bus>, rs: usize, rt: usize, rd: usize, _sa: u32) {
+    debug!(
+        "{:08X} DSRLV {}, {}, {}",
+        core.pc, REGS[rd], REGS[rt], REGS[rs]
+    );
+    let result = core.getd(rt) >> (core.getd(rs) & 63);
+    core.setd(rd, result);
+}
+
+pub fn dsrav(core: &mut Core<impl Bus>, rs: usize, rt: usize, rd: usize, _sa: u32) {
+    debug!(
+        "{:08X} DSRAV {}, {}, {}",
+        core.pc, REGS[rd], REGS[rt], REGS[rs]
+    );
+    let result = (core.getd(rt) as i64 >> (core.getd(rs) & 63)) as u64;
     core.setd(rd, result);
 }
 
