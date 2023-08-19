64211e0010453aa266881fb8c21890e789b3a353
James Lippitt <james.lippitt@gmail.com>
2023-08-19 11:29:30 +0100

    Vertically centre the canvas

--- utopia-web/src/components/Canvas.tsx
+++ utopia-web/src/components/Canvas.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
     display: flex;
     justify-content: center;
-    align-items: top;
+    align-items: center;
     height: 100%;
 `;
 
