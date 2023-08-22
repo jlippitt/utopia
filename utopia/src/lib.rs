b2aba91db6ba5d6809e0edbd6c952e48b712de42
James Lippitt <james.lippitt@gmail.com>
2023-08-22 08:34:09 +0100

    Disable 'helpful' clippy warnings that would actually make the codebase worse/harder to read

--- utopia/src/lib.rs
+++ utopia/src/lib.rs
@@ -1,3 +1,13 @@
+#![allow(
+    clippy::collapsible_else_if,
+    clippy::comparison_chain,
+    clippy::identity_op,
+    clippy::manual_range_contains,
+    clippy::match_single_binding,
+    clippy::neg_multiply,
+    clippy::single_match
+)]
+
 pub use system::*;
 
 pub mod core;
