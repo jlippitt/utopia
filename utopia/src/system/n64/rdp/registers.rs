727f1b19069c4f2559535d29d5c593cc29d4ae62
James Lippitt <james.lippitt@gmail.com>
2023-08-22 16:02:07 +0100

    Populate DP_START and DP_END registers

--- utopia/src/system/n64/rdp/registers.rs
+++ utopia/src/system/n64/rdp/registers.rs
@@ -1,4 +1,6 @@
+use super::super::dma::Dma;
 use bitfield_struct::bitfield;
+use tracing::debug;
 
 #[bitfield(u32)]
 struct Status {
@@ -18,6 +20,9 @@ struct Status {
 }
 
 pub struct Registers {
+    dma_requested: Option<Dma>,
+    start: u32,
+    end: u32,
     status: Status,
 }
 
@@ -35,12 +40,17 @@ impl Registers {
 
     pub fn new() -> Self {
         Self {
+            dma_requested: None,
+            start: 0,
+            end: 0,
             status: Status::new(),
         }
     }
 
     pub fn get(&self, index: usize) -> u32 {
         match index {
+            0 => self.start,
+            1 => self.end,
             3 => self.status.into(),
             _ => unimplemented!("RDP Command Register Read: {}", Self::NAMES[index]),
         }
@@ -48,6 +58,23 @@ impl Registers {
 
     pub fn set(&mut self, index: usize, value: u32) {
         match index {
+            0 => {
+                self.start = value & 0x00ff_ffff;
+                debug!("DP_START: {:08X}", self.start);
+                self.status.set_start_pending(true);
+            }
+            1 => {
+                self.end = value & 0x00ff_ffff;
+                debug!("DP_END: {:08X}", self.end);
+                self.status.set_start_pending(false);
+
+                self.dma_requested = Some(Dma {
+                    src_addr: self.start,
+                    dst_addr: 0, // Not used
+                    len: self.end - self.start,
+                    reverse: false, // Not used
+                })
+            }
             _ => unimplemented!(
                 "RDP Command Register Write: {} <= {:08X}",
                 Self::NAMES[index],
