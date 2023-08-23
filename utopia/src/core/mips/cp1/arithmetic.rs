b23e5b679df332d2fa8731fdfcefa835165ac684
James Lippitt <james.lippitt@gmail.com>
2023-08-23 09:09:09 +0100

    Implement SQRT, ABS and NEG

--- utopia/src/core/mips/cp1/arithmetic.rs
+++ utopia/src/core/mips/cp1/arithmetic.rs
@@ -23,7 +23,22 @@ pub fn div_s(core: &mut Core<impl Bus>, ft: usize, fs: usize, fd: usize) {
     core.cp1.set_s(fd, core.cp1.s(fs) / core.cp1.s(ft));
 }
 
+pub fn sqrt_s(core: &mut Core<impl Bus>, _ft: usize, fs: usize, fd: usize) {
+    debug!("{:08X} SQRT.S $F{}, $F{}", core.pc, fd, fs);
+    core.cp1.set_s(fd, core.cp1.s(fs).sqrt());
+}
+
+pub fn abs_s(core: &mut Core<impl Bus>, _ft: usize, fs: usize, fd: usize) {
+    debug!("{:08X} ABS.S $F{}, $F{}", core.pc, fd, fs);
+    core.cp1.set_s(fd, core.cp1.s(fs).abs());
+}
+
 pub fn mov_s(core: &mut Core<impl Bus>, _ft: usize, fs: usize, fd: usize) {
     debug!("{:08X} MOV.S $F{}, $F{}", core.pc, fd, fs);
     core.cp1.set_s(fd, core.cp1.s(fs));
+}
+
+pub fn neg_s(core: &mut Core<impl Bus>, _ft: usize, fs: usize, fd: usize) {
+    debug!("{:08X} NEG.S $F{}, $F{}", core.pc, fd, fs);
+    core.cp1.set_s(fd, -core.cp1.s(fs));
 }
