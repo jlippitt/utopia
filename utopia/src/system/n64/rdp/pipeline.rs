6aa6cf6376edef690391779291a7e5f5d694532b
James Lippitt <james.lippitt@gmail.com>
2023-09-04 09:07:18 +0100

    Partial decode of some more commands

--- utopia/src/system/n64/rdp/pipeline.rs
+++ utopia/src/system/n64/rdp/pipeline.rs
@@ -18,8 +18,16 @@ impl Pipeline {
 
         while let Some(&word) = iter.next() {
             match (word >> 56) as u8 {
+                0x08 => debug!(
+                    "FillTriangle {:016X} {:016X} {:016X}",
+                    iter.next().unwrap(),
+                    iter.next().unwrap(),
+                    iter.next().unwrap(),
+                ),
                 0x24 => debug!("TextureRectangle {:016X}", iter.next().unwrap()),
+                0x27 => debug!("SyncPipe"),
                 0x28 => debug!("SyncTile"),
+                0x29 => debug!("SyncFull"),
                 0x2d => debug!("SetScissor"),
                 0x2f => debug!("SetOtherModes"),
                 0x30 => debug!("LoadTlut"),
@@ -27,6 +35,7 @@ impl Pipeline {
                 0x35 => op::SetTile::from(word).exec(self, rdram, ctx),
                 0x36 => debug!("FillRectangle"),
                 0x37 => debug!("SetFillColor"),
+                0x39 => debug!("SetBlendColor"),
                 0x3c => debug!("SetCombineMode"),
                 0x3d => op::SetTextureImage::from(word).exec(self, rdram, ctx),
                 0x3f => debug!("SetColorImage"),
