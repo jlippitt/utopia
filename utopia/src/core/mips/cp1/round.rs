8c3e605bb393f28cc26c1db22bfdc4fd7b1225e0
James Lippitt <james.lippitt@gmail.com>
2023-09-04 08:50:38 +0100

    Implement round/ceil/floor

--- utopia/src/core/mips/cp1/round.rs
+++ utopia/src/core/mips/cp1/round.rs
@@ -1,7 +1,22 @@
 use super::super::{Bus, Core};
 use tracing::debug;
 
+pub fn round_w_s(core: &mut Core<impl Bus>, _ft: usize, fs: usize, fd: usize) {
+    debug!("{:08X} ROUND.W.S $F{}, $F{}", core.pc, fd, fs);
+    core.cp1.set_w(fd, core.cp1.s(fs).round() as i32);
+}
+
 pub fn trunc_w_s(core: &mut Core<impl Bus>, _ft: usize, fs: usize, fd: usize) {
     debug!("{:08X} TRUNC.W.S $F{}, $F{}", core.pc, fd, fs);
     core.cp1.set_w(fd, core.cp1.s(fs).trunc() as i32);
+}
+
+pub fn ceil_w_s(core: &mut Core<impl Bus>, _ft: usize, fs: usize, fd: usize) {
+    debug!("{:08X} CEIL.W.S $F{}, $F{}", core.pc, fd, fs);
+    core.cp1.set_w(fd, core.cp1.s(fs).ceil() as i32);
+}
+
+pub fn floor_w_s(core: &mut Core<impl Bus>, _ft: usize, fs: usize, fd: usize) {
+    debug!("{:08X} FLOOR.W.S $F{}, $F{}", core.pc, fd, fs);
+    core.cp1.set_w(fd, core.cp1.s(fs).floor() as i32);
 }
