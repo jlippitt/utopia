b745b49129102cf9d354d157570a4e6699600295
James Lippitt <james.lippitt@gmail.com>
2023-10-29 12:14:55 +0000

    Further decode VDP register writes

--- utopia/src/system/sms/vdp.rs
+++ utopia/src/system/sms/vdp.rs
@@ -1,35 +1,50 @@
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
-use tracing::warn;
+use tracing::{trace, warn};
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, FromPrimitive)]
 enum Command {
-    VramRead = 0,
-    VramWrite = 1,
-    RegisterWrite = 2,
-    CramWrite = 3,
+    ReadVram = 0,
+    WriteVram = 1,
+    WriteRegister = 2,
+    WriteCram = 3,
 }
 
 pub struct Vdp {
+    command: Command,
+    address: u16,
     write_buffer: Option<u8>,
 }
 
 impl Vdp {
     pub fn new() -> Self {
-        Self { write_buffer: None }
+        Self {
+            command: Command::ReadVram,
+            address: 0,
+            write_buffer: None,
+        }
     }
 
     pub fn write_command(&mut self, value: u8) {
         if let Some(low) = self.write_buffer.take() {
-            let command = Command::from_u8(value >> 6).unwrap();
-            let address = u16::from_le_bytes([low, value & 0x3f]);
-            warn!("VDP Command: {:?} {:04X}", command, address);
+            self.command = Command::from_u8(value >> 6).unwrap();
+            self.address = u16::from_le_bytes([low, value & 0x3f]);
+
+            if self.command == Command::WriteRegister {
+                self.write_register((self.address >> 8) as u8 & 15, self.address as u8);
+            }
         } else {
             self.write_buffer = Some(value)
         }
     }
 
     pub fn write_data(&mut self, value: u8) {
-        warn!("VDP Data: {:02X}", value);
+        trace!("VDP Data: {:02X}", value);
+    }
+
+    fn write_register(&mut self, reg: u8, value: u8) {
+        match reg {
+            _ => warn!("Unmapped VDP Register Write: {:02X} <= {:02X}", reg, value),
+        }
     }
 }
