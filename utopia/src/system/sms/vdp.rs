80173a93a7299be07a4cb878dac135a79aca90c4
James Lippitt <james.lippitt@gmail.com>
2023-10-29 11:56:44 +0000

    Decode VDP commands

--- utopia/src/system/sms/vdp.rs
+++ utopia/src/system/sms/vdp.rs
@@ -1,16 +1,32 @@
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
 use tracing::warn;
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq, FromPrimitive)]
+enum Command {
+    VramRead = 0,
+    VramWrite = 1,
+    RegisterWrite = 2,
+    CramWrite = 3,
+}
+
 pub struct Vdp {
-    //
+    write_buffer: Option<u8>,
 }
 
 impl Vdp {
     pub fn new() -> Self {
-        Self {}
+        Self { write_buffer: None }
     }
 
     pub fn write_command(&mut self, value: u8) {
-        warn!("VDP Command: {:02X}", value);
+        if let Some(low) = self.write_buffer.take() {
+            let command = Command::from_u8(value >> 6).unwrap();
+            let address = u16::from_le_bytes([low, value & 0x3f]);
+            warn!("VDP Command: {:?} {:04X}", command, address);
+        } else {
+            self.write_buffer = Some(value)
+        }
     }
 
     pub fn write_data(&mut self, value: u8) {
