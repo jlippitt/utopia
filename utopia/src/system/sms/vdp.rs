3a3acdf8a7a0f868c48348da69968e7df9ae4bec
James Lippitt <james.lippitt@gmail.com>
2023-10-29 12:45:27 +0000

    Set the VBlank line from the selected VDP mode

--- utopia/src/system/sms/vdp.rs
+++ utopia/src/system/sms/vdp.rs
@@ -14,6 +14,7 @@ pub struct Vdp {
     command: Command,
     address: u16,
     mode_select: u8,
+    vblank_line: u16,
     write_buffer: Option<u8>,
 }
 
@@ -23,6 +24,7 @@ impl Vdp {
             command: Command::ReadVram,
             address: 0,
             mode_select: 0,
+            vblank_line: 192,
             write_buffer: None,
         }
     }
@@ -49,19 +51,36 @@ impl Vdp {
 
         match reg {
             0x00 => {
-                self.mode_select =
-                    (self.mode_select & 0b0101) | (value & 0b0010) | ((value & 0b0100) << 1);
-
-                warn!("Mode Select: {:04b}", self.mode_select);
+                self.update_mode(
+                    (self.mode_select & 0b0101) | (value & 0b0010) | ((value & 0b0100) << 1),
+                );
             }
             0x01 => {
-                self.mode_select = (self.mode_select & 0b1010)
-                    | ((value & 0b1_0000) >> 4)
-                    | ((value & 0b1000) >> 1);
-
-                warn!("Mode Select: {:04b}", self.mode_select);
+                self.update_mode(
+                    (self.mode_select & 0b1010)
+                        | ((value & 0b1_0000) >> 4)
+                        | ((value & 0b1000) >> 1),
+                );
             }
             _ => (),
         }
+    }
+
+    fn update_mode(&mut self, mode_select: u8) {
+        self.mode_select = mode_select;
+        trace!("Mode Select: {:04b}", self.mode_select);
+
+        if (mode_select & 0b1000) == 0 {
+            unimplemented!("TMS9918 Modes");
+        }
+
+        self.vblank_line = match mode_select & 0b111 {
+            0b1001 | 0b1101 => panic!("Invalid video mode: {:04b}", mode_select),
+            0b1110 => 240,
+            0b1011 => 224,
+            _ => 192,
+        };
+
+        trace!("VBlank Line: {}", self.vblank_line);
     }
 }
