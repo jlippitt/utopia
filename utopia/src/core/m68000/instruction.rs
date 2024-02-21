834577d17b273e17e68f15d5a5fb82c64d529556
James Lippitt <james.lippitt@gmail.com>
2024-02-21 17:43:44 +0000

    Implement CMP

--- utopia/src/core/m68000/instruction.rs
+++ utopia/src/core/m68000/instruction.rs
@@ -1,4 +1,5 @@
 pub use bit::*;
+pub use compare::*;
 pub use control::*;
 pub use meta::*;
 pub use transfer::*;
@@ -11,6 +12,7 @@ use tracing::trace;
 
 mod address_mode;
 mod bit;
+mod compare;
 mod control;
 mod meta;
 mod transfer;
@@ -21,15 +23,4 @@ pub fn reset(core: &mut Core<impl Bus>) {
     core.set_areg::<u32>(7, core.read(0x0000_0000));
     core.set_pc(core.read(0x0000_0004));
     core.set_int_level(7);
-}
-
-pub fn tst<T: Size>(core: &mut Core<impl Bus>, word: u16) {
-    let operand = AddressMode::from(word);
-    trace!("TST.{} {}", T::NAME, operand);
-    let value: T = operand.read(core);
-    core.set_ccr(|flags| {
-        flags.set_nz(value);
-        flags.v = false;
-        flags.c = false;
-    });
 }
---
+++ utopia/src/core/m68000/instruction/compare.rs
@@ -0,0 +1,33 @@
+use super::{AddressMode, Bus, Core, Size};
+use tracing::trace;
+
+fn compare<T: Size>(core: &mut Core<impl Bus>, lhs: T, rhs: T) {
+    let result = lhs.wrapping_sub(&rhs);
+    let carries = lhs ^ rhs ^ result;
+    let overflow = (lhs ^ result) & (rhs ^ lhs);
+    core.set_ccr(|flags| {
+        flags.set_nz(result);
+        flags.v = (overflow & T::SIGN_BIT) != T::zero();
+        flags.c = ((carries ^ overflow) & T::SIGN_BIT) != T::zero();
+    });
+}
+
+pub fn cmp<T: Size>(core: &mut Core<impl Bus>, word: u16) {
+    let src = AddressMode::from(word);
+    let dst = (word >> 9) & 7;
+    trace!("CMP.{} D{}, {}", T::NAME, dst, src);
+    let src_value: T = src.read(core);
+    let dst_value = core.dreg(dst as usize);
+    compare(core, dst_value, src_value);
+}
+
+pub fn tst<T: Size>(core: &mut Core<impl Bus>, word: u16) {
+    let operand = AddressMode::from(word);
+    trace!("TST.{} {}", T::NAME, operand);
+    let value: T = operand.read(core);
+    core.set_ccr(|flags| {
+        flags.set_nz(value);
+        flags.v = false;
+        flags.c = false;
+    });
+}
